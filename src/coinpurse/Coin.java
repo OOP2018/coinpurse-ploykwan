f50d2bed00d1d673901772d4ec27d1f97ce1eb85
Kwankaew Uttama <kwankaew.u@ku.th>
2018-01-19 16:30:00 +0700

    edit Cion class

--- src/coinpurse/Coin.java
+++ src/coinpurse/Coin.java
@@ -24,13 +24,6 @@ public class Coin implements Comparable<Coin>{
 	}
 	
 	public int compareTo(Coin coin){
-//		if( this.getValue() > coin.getValue() ){
-//			return -1;
-//		}
-//		else if (this.getValue() < coin.getValue()){
-//			return 1;
-//		}
-//		else return 0;
 		return (int)(Math.signum(coin.getValue() - this.getValue()));
 		
 	}
@@ -54,22 +47,6 @@ public class Coin implements Comparable<Coin>{
 	public void setCurrency(String currency) {
 		this.currency = currency;
 	}
-	
-	
-	
-	public static void main(String[] arg){
-		List<Coin> coins = new ArrayList<Coin>();
-		
-		coins.add(new Coin(10.0,"B"));
-		coins.add(new Coin(15.0,"B"));
-		coins.add(new Coin(20.0,"B"));
-		coins.add(new Coin(70.0,"B"));
-		coins.add(new Coin(35.0,"B"));
-		coins.add(new Coin(40.0,"B"));
-		
-		System.out.println(coins);
-		java.util.Collections.sort(coins);
-		System.out.println(coins);
-	}
+
 }
 
