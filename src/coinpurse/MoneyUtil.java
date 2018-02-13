c9c9e5da90ac7fbc13dfee48dac340c882149227
Kwankaew Uttama <kwankaew.u@ku.th>
2018-02-13 22:48:17 +0700

    edit filterByCurrency

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -13,26 +13,6 @@ import java.util.List;
  *
  */
 public class MoneyUtil {
-	/**
-	 * Return a List of the elements from the parameter List (money) with currency
-	 * that is the same as the currency parameter. If no matches, return an empty
-	 * list.
-	 * 
-	 * @param money
-	 *            is the list of money in purse.
-	 * @param currency
-	 *            is the currency we want. Must not be null.
-	 * @return the list of money that same currency.
-	 */
-	public static List<Valuable> filterByCurency(List<Valuable> money, String currency) {
-		List<Valuable> sameCurrency = null;
-		for (Valuable c : money) {
-			if (currency.equalsIgnoreCase(c.getCurrency())) {
-				sameCurrency.add(c);
-			}
-		}
-		return sameCurrency;
-	}
 
 	/**
 	 * The method is sort the money from most valuable.
